bff880a5060e56f17e5ae24dd3b43e95e01e0bae
Vivek Chavan <111511821+vivekchavan14@users.noreply.github.com>
2022-12-11 23:52:17 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,12 +1,10 @@
-https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
-
 class Solution {
-    public List<Integer> findDisappearedNumbers(int[] arr) {
-        int i = 0;
-        while(i < arr.length)
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i=0;
+        while(i<arr.length)
         {
             int correct = arr[i]-1;
-            if(arr[i]!=arr[correct])
+            if(arr[i]<arr.length && arr[i]!=correct)
             {
                 swap(arr,i,correct);
             }
@@ -15,20 +13,20 @@ class Solution {
                 i++;
             }
         }
-        List<Integer> ans = new ArrayList<>();
-        for(int index = 0; index < arr.length; index++)
+        List<integer> ans = new ArrayList<>();
+        for(int index = 0;index<arr.length;index++)
         {
             if(arr[index]!=index+1)
             {
                 ans.add(index+1);
             }
+            return ans;
         }
-        return ans;
-    }
-    void swap(int[] arr,int start,int end)
-    {
-        int temp = arr[start];
-        arr[start]=arr[end];
-        arr[end]=temp;
+        static void swap(int[] arr,int start,int end)
+        {
+            int temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+        }
     }
 }
