0fcd3b0de923287a93faea00b0c74d4349e1dec8
Vivek Chavan <111511821+vivekchavan14@users.noreply.github.com>
2022-12-23 14:29:07 +0530

    Add files via upload

---
+++ BinarySearchRecursion.java
@@ -0,0 +1,26 @@
+//Binary Search using recursion
+
+public class BinarySearchRecursion {
+    public static void main(String[] args) {
+        int[] arr = {2,4,3,5,6,8};
+        int target = 5;
+        System.out.println(search(arr,target,0,arr.length-1));
+    }
+    static int search(int[] arr,int target,int start,int end)
+    {
+        if(start>end)
+        {
+            return -1;
+        }
+        int mid = start + (end-start)/2;
+        if(arr[mid]==target)
+        {
+            return mid;
+        }
+        if(target < arr[mid])
+        {
+            return search(arr,target,start,mid-1);
+        }
+        return search(arr,target,mid+1,end);
+    }
+}
